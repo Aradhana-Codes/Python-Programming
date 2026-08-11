1d0e252c53a71030807bb15518242ccfd711c1bd
Aradhana <aradhanabopche99@gmail.com>
2026-08-11 08:13:02 +0530

    non-repeating character

---
+++ FirstNonRepeating.py
@@ -0,0 +1,18 @@
+"""
+Find the first character that appears only once in a given string.
+Example: input:"aabbcddee"
+         output:"c"
+ """
+
+text="aabbcddee"
+
+frequency={}
+
+for char in text:
+    frequency[char]=frequency.get(char,0)+1
+
+for char in text:
+    if frequency[char]==1:
+        print("First non-repeating character:",char)
+        break
+
