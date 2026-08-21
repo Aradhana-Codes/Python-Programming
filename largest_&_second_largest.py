67e5c36f058b95f4e06bae8e044f0cf8c180f62d
Aradhana <aradhanabopche99@gmail.com>
2026-08-21 14:14:00 +0530

    largest&SecondLargest_from list

---
+++ largest_&_second_largest.py
@@ -0,0 +1,21 @@
+"""
+Find he largest and second largest elements in a list.
+Example: input: [10,25,7,45,18]
+         output: 
+         Largest element: 45
+         Second largest element: 25
+"""
+
+numbers=[10,25,7,45,18]
+
+largest=numbers[0]
+second_largest=numbers[0]
+
+for number in numbers:
+    if number>largest:
+        second_largest=largest
+        largest=number
+    elif number>second_largest and number!=largest:
+        second_largest=number
+print("Largest element:",largest)
+print("Second largest element:",second_largest)
